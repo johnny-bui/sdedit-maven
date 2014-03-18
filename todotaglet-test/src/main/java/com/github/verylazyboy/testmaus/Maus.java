311c29517577b5a5946117746b3ec0b802a0625a
Your Name <you@example.com>
2014-03-18 15:58:45 +0100

    add dir resource to src/main/resources

---
+++ todotaglet-test/src/main/java/com/github/verylazyboy/testmaus/Maus.java
@@ -0,0 +1,15 @@
+package com.github.verylazyboy.testmaus;
+
+/**
+ * 
+ * 	@sequence.diagram
+ *  a:A
+ *  /decoder:EnigmaDecoder
+ * 
+ *  a:decoder.new
+ * 
+ * @author hbui
+ */
+public class Maus {
+	
+}
